c4407a9258a283971fe2e8feae654f61dcd33f3b
shawonahmed1 <mdshawon356@gmail.com>
2020-08-21 16:24:50 +0600

    with dynamic name from object and component pass from props with Document function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
+
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ const nayok = ['Shawon Ahmed', 'Shafin Ahmed', 'Shahrukh Ahmed', 'Shahin Ahmed'];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           Welcome Mr.Shawon in the React APP for Hacking the coding system <code>src/App.js</code> and save to reload.
-        </p>
-        
+        <Document name={nayok[0]}select="Hero"></Document>
+        <Document name={nayok[1]} select="Freelancer"></Document>
+        <Document name={nayok[2]} select="Singer"></Document>
+        <Document name={nayok[3]} select="Dancer"></Document>
+     
       </header>
     </div>
   );
 }
 
+function Document(props){
+return (
+  <div style={{border:"1px solid red", margin:"10px", background:"skyblue", borderRadius:"20px"}}>
+    <h1 style={{color:"tomato", background:"yellow"}}>I am {props.name}</h1>
+    <h2>{props.select} of the year</h2>
+  </div>
+  
+ )
+}
 export default App;
