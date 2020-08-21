5fb7cf07fe613c6cdf980a4265eb22843a546d14
shawonahmed1 <mdshawon356@gmail.com>
2020-08-21 07:57:27 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+           Welcome Mr.Shawon in the React APP for Hacking the coding system <code>src/App.js</code> and save to reload.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
